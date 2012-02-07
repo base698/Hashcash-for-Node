187340e52613495ad4d5363a409214c424e8211d
Justin Thomas <justin@chromatikmusic.com>
2012-02-06 17:25:24 -0800

    Session support instead

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var hashcash = require('./lib/hashcash');
 var app = express.createServer();
+app.use(express.cookieParser());
+app.use(express.session({ secret: "keyboard cat" }));
 app.use(express.static('lib'));
 app.get('/protected',hashcash.middleware(),function(req,res) {
 	res.send('Hello, World!');
