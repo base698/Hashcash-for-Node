6d684fbc04d663b44da09bf79a40a337bf453bce
Justin Thomas <justin.thomas1@gmail.com>
2014-04-11 15:03:38 -0700

    use local

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ var hashcash = require('./');
 var app = express();
 app.use(express.cookieParser());
 app.use(express.session({ secret: "keyboard cat" }));
-app.use(express.static('lib'));
+app.use(express.static('./'));
 
 app.get('/protected',hashcash(),function(req,res) {
 	res.send('Hello, World!');
