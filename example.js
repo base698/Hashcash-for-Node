ebc18536f813731f4ccfe24173aab8bcf6b1c7d7
Justin Thomas <justin.thomas1@gmail.com>
2014-04-11 12:01:59 -0700

    server updated for express v3

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var hashcash = require('./lib/hashcash');
-var app = express.createServer();
+var hashcash = require('./');
+var app = express();
 app.use(express.cookieParser());
 app.use(express.session({ secret: "keyboard cat" }));
 app.use(express.static('lib'));
-app.get('/protected',hashcash.middleware(),function(req,res) {
+
+app.get('/protected',hashcash(),function(req,res) {
 	res.send('Hello, World!');
 });
 
@@ -12,5 +13,6 @@ app.get('/unprotected',function(req,res) {
 	res.send('You should use protection.');
 });
 
-console.log("Listening...");
-app.listen(1337);
+var port = 1337;
+console.log("Listening on: " + port);
+app.listen(port);
